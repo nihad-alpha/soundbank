49fd150da6ac0b2608fd840e4de738f66e0a97c4
nihadsuvalija <nihadsuvalija@gmail.com>
2021-03-10 14:25:28 +0100

    Created SongDao

--- api/dao/SongDao.class.php
+++ api/dao/SongDao.class.php
@@ -1,32 +1,24 @@
 <?php
 class SongDao extends BaseDao {
 
-    public function get_song_by_id() {
-        
+    public function get_song_by_id($id) {
+        return $this->query("SELECT * FROM songs WHERE song_id = :id", ["id" => $id]);
     }
 
-    public function get_song_by_artist_id() {
-
+    public function get_song_by_artist_id($id) {
+        return $this->query("SELECT * FROM songs WHERE artist_id = :id", ["id" => $id]);
     }
 
-    public function get_song_by_name() {
-
+    public function get_song_by_name($song_name) {
+        return $this->query("SELECT * FROM songs WHERE song_name = :song_name", ["song_name" => $song_name]);
     }
 
-    public function get_song_length() {
-
+    public function get_song_length_by_id($id) {
+        return $this->query("SELECT length FROM songs WHERE song_id = :id", ["id" => $id]);
     }
 
-    public function get_album_id() {
-
-    }
-
-    public function get_album_name() {
-
-    }
-
-    public function get_genre() {
-        
+    public function get_album_id_of_song_id($id) {
+        return $this->query("SELECT album_id FROM songs WHERE song_id = :id", ["id" => $id]);
     }
 }
 ?>
