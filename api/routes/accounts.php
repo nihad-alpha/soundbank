7fe5eb9cef6f9a08ab4e0d5c136cb93128a3059e
nihad-alpha <nihad.suvalija@stu.ibu.edu.ba>
2021-04-02 18:59:08 +0200

    Added more routes for Accounts

--- api/routes/accounts.php
+++ api/routes/accounts.php
@@ -14,4 +14,15 @@ Flight::route("GET /accounts/@id", function($id) {
     Flight::json(Flight::accountDao()->get_account_by_id($id));
 });
 
+Flight::route("POST /accounts", function() {
+    $request = Flight::request();
+    Flight::accountDao()->add_account($request->data->getData());
+});
+
+Flight::route("PUT /accounts/@id", function($id){
+    $data = Flight::request()->data->getData();
+    Flight::accountDao()->update_account($id, $data);
+    Flight::json(Flight::accountDao()->get_account_by_id($id));
+});
+
 ?>
