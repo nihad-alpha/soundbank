301cfcdc09a2ae24bac06edb50de58f7b41d47db
nihad-alpha <nihad.suvalija@stu.ibu.edu.ba>
2021-04-03 19:06:41 +0200

    Cleaned up the code and added more comments

--- api/routes/accounts.php
+++ api/routes/accounts.php
@@ -7,7 +7,6 @@ error_reporting(E_ALL);
 require_once dirname(__FILE__)."/../dao/AccountDao.class.php";
 require_once dirname(__FILE__)."/../services/AccountService.class.php";
 
-// Method for searching accounts by name.
 Flight::route("GET /accounts", function() {
     $search = Flight::query('search');
     $offset = Flight::query('offset', 0);
@@ -17,23 +16,19 @@ Flight::route("GET /accounts", function() {
     
 });
 
-// Getting accounts by id from the database.
 Flight::route("GET /accounts/@id", function($id) {
     Flight::json(Flight::accountService()->get_account_by_id($id));
 });
 
-// Getting accounts by username from the database.
 Flight::route("GET /accounts/@username", function($username) {
     Flight::json(Flight::accountService()->get_account_by_username($username));
 });
 
-// Inserting accounts into the database.
 Flight::route("POST /accounts", function() {
     $request = Flight::request();
     Flight::accountService()->add_account($request->data->getData());
 });
 
-// Updating existing accounts in the database.
 Flight::route("PUT /accounts/@id", function($id){
     $data = Flight::request()->data->getData();
     Flight::accountService()->update_account_by_id($id, $data);
--- api/routes/albums.php
+++ api/routes/albums.php
@@ -1,7 +1,6 @@
 <?php
 require_once dirname(__FILE__)."/../dao/AlbumDao.class.php";
 
-// Method for searching albums by name.
 Flight::route("GET /albums", function() {
     $search = Flight::query('search');
     $offset = Flight::query('offset', 0);
@@ -10,18 +9,15 @@ Flight::route("GET /albums", function() {
     Flight::json(Flight::albumService()->search_albums($search, $offset, $limit));
 });
 
-// Getting albums by id from the database.
 Flight::route("GET /albums/@id", function($id) {
     Flight::json(Flight::albumService()->get_album_by_id($id));
 });
 
-// Inserting new albums into the database.
 Flight::route("POST /albums", function() {
     $request = Flight::request();
     Flight::albumService()->add_album($request->data->getData());
 });
 
-// Updating existing albums in the database.
 Flight::route("PUT /albums/@id", function($id) {
     $data = Flight::request()->data->getData();
     Flight::albumService()->update_album($id, $data);
