df52e56723ef7db27c0327045926b57b3379371e
nihad-alpha <nihad.suvalija@stu.ibu.edu.ba>
2021-06-27 14:54:43 +0200

    updated database

--- api/config.php
+++ api/config.php
@@ -3,10 +3,15 @@
 class Config {
     const DATE_FORMAT = "Y-m-d H:i:s";
 
-    const DB_HOST = "localhost";
+    /*const DB_HOST = "localhost";
     const DB_USERNAME = "root";
     const DB_PASSWORD = "berina123";
-    const DB_NAME = "Soundbank";
+    const DB_NAME = "Soundbank";*/
+
+    const DB_HOST = "soundbank-db-do-user-9216819-0.b.db.ondigitalocean.com";
+    const DB_USERNAME = "il2eoqw5yy0uf4qs";
+    const DB_PASSWORD = "berina123";
+    const DB_NAME = "soundbank";
 
     const SMTP_HOST = "smtp.mailgun.org";
     const SMTP_PORT = 587;
